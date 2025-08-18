94b6478e0889d9dfd388e77aade484e2a892bece
Sahil Deshmukh <61318257+sahildmk@users.noreply.github.com>
2025-08-18 22:42:51 +1000

    feat(home): enhance introduction section with improved animations and add WIP timeline component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import MongoDB from "./components/mongo";
 
 const blurStyle =
-  "motion-opacity-in-[10%] motion-blur-in-[5px] motion-duration-[2s] motion-ease-in-out";
+  "motion-opacity-in-[5%] motion-blur-in-[5px] motion-duration-[2s] motion-ease-in-out";
 
 export default function Home() {
   return (
     <main className="flex min-h-svh flex-col items-center justify-between p-6  text-stone-300 bg-stone-900">
-      <div className="md:w-xl lg:w-3xl mt-30 space-y-10">
+      <div className="md:w-xl lg:w-3xl mt-40 sm:space-y-10 space-y-10">
         <section className="text-stone-300">
           <h1
             className={`text-3xl font-bold ${blurStyle} motion-delay-200 font-serif`}
@@ -17,16 +17,27 @@ export default function Home() {
             <span className={`${blurStyle} motion-delay-300`}>
               Software Engineer at
             </span>
-            <span className={`${blurStyle} motion-delay-400`}>
-              <MongoDB className={`size-6 inline-block `} />
-              <span className="">MongoDB</span>
-            </span>
+            <a
+              className={`inline-block ${blurStyle} motion-delay-400 group hover:cursor-pointer`}
+              href="https://www.mongodb.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <MongoDB
+                className={`size-6 inline-block group-hover:scale-120 transition-transform group-hover:rotate-20 duration-300`}
+              />
+              <p className="group-hover:scale-105 inline-block transition-all duration-300 ">
+                MongoDB
+              </p>
+            </a>
           </div>
         </section>
         <section className="space-y-4">
           <div className="flex space-x-2">
             <p className={`${blurStyle} motion-delay-500`}>Hi</p>
-            <p className={`${blurStyle} motion-delay-500 motion-preset-shake`}>
+            <p
+              className={`${blurStyle} motion-delay-500 motion-preset-seesaw-lg`}
+            >
               👋
             </p>
           </div>
@@ -44,3 +55,22 @@ export default function Home() {
     </main>
   );
 }
+
+const Timeline = () => {
+  return (
+    <div>
+      <section>
+        <div className="flex items-center space-x-2">
+          <MongoDB
+            className={`h-8 inline-block ${blurStyle} motion-delay-800`}
+          />
+          <h2
+            className={`text-2xl font-medium font-serif ${blurStyle} motion-delay-800`}
+          >
+            MongoDB
+          </h2>
+        </div>
+      </section>
+    </div>
+  );
+};
