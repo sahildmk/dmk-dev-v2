cbfb12b89f3f4586546eecc3f4b56e8685777c65
Sahil Deshmukh <61318257+sahildmk@users.noreply.github.com>
2025-08-21 12:11:14 +1000

    feat(home): re-arrange and add RM work

--- app/page.tsx
+++ app/page.tsx
@@ -24,31 +24,44 @@ export default function Home() {
             </p>
           </div>
           <p className={`${blurStyle} motion-delay-600`}>
+            My name is <span className="font-serif font-semibold">Sahil</span>.
+          </p>
+          <div className={`${blurStyle} motion-delay-700`}>
+            <span>I&apos;m currently building </span>
+            <p className="inline-block motion-opacity-loop-60 motion-duration-[3s] motion-ease-in-out">
+              <a
+                href="https://www.mongodb.com/products/tools/relational-migrator"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                some cool things
+              </a>
+            </p>
+            <span> at </span>
+            <p className="inline-block">
+              <a
+                className={`inline-block group hover:cursor-pointer`}
+                href="https://www.mongodb.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <MongoDB
+                  className={`h-6 inline-block group-hover:mr-1 motion-preset-seesaw-md group-hover:scale-120 transition-all group-hover:rotate-20 duration-300`}
+                />
+                <span className="group-hover:scale-105 font-serif font-semibold inline-block transition-all duration-300 ">
+                  MongoDB.
+                </span>
+              </a>
+            </p>
+          </div>
+          <p className={`${blurStyle} motion-delay-800`}>
             I love solving meaningful problems. I also love software. Naturally,
             I keep myself busy by solving meaningful problems with software.
           </p>
-          <p className={`${blurStyle} motion-delay-700`}>
+          <p className={`${blurStyle} motion-delay-900`}>
             I also love learning from all the wonderfully smart and talented
             people around me, and finding ways to share all the cool things
             I&apos;ve learnt (I&apos;m working on being better at this).
-          </p>
-          <p>
-            <span className={`${blurStyle} motion-delay-800 mr-1`}>
-              I&apos;m currently a Software Engineer at
-            </span>
-            <a
-              className={`inline-block ${blurStyle} motion-delay-900 group hover:cursor-pointer`}
-              href="https://www.mongodb.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <MongoDB
-                className={`h-6 inline-block group-hover:mr-1 motion-preset-seesaw-md group-hover:scale-120 transition-all group-hover:rotate-20 duration-300`}
-              />
-              <span className="group-hover:scale-105 inline-block transition-all duration-300 ">
-                MongoDB
-              </span>
-            </a>
           </p>
         </section>
       </div>
